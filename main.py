ad546cc54e8f00d7c454e91759decea268f43e01
yxfhy <yxfhy303@gmail.com>
2025-05-01 09:58:48 +0000

    test2

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 def add(a: int, b: int) -> int:
+    """#this is comment
+    This function adds two numbers.
+    Args:
+        a (int): The first number.
+        b (int): The second number."""
     return a + b
